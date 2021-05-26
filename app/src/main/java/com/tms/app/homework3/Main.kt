164f46ad42ef28e33e5a9507339d4f19d7ca4266
Dmitry1102 <meleshkevich2016@mail.ru>
2021-05-26 12:21:53 +0200

    Correct mistakes

--- app/src/main/java/com/tms/app/homework3/Main.kt
+++ app/src/main/java/com/tms/app/homework3/Main.kt
@@ -3,7 +3,7 @@ package com.tms.app.homework3
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.tms.app.R
-import com.tms.app.homework2.Singleton
+
 
 class Main: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
