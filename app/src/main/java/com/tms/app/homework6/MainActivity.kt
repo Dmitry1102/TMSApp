d4dbec42dd1b263c699e72887af1709f92f40ce5
Dmitry1102 <meleshkevich2016@mail.ru>
2021-05-28 15:36:21 +0200

    Homework 5

---
+++ app/src/main/java/com/tms/app/homework6/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.tms.app.homework6
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.tms.app.R
+
+class MainActivity: AppCompatActivity() {
+    private lateinit var textView: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val intent = Intent(this, Homework6Activity::class.java)
+        startActivity(intent)
+    }
+}
