0703415fd03e301280b289a88c025d3f95c1100d
Dmitry1102 <meleshkevich2016@mail.ru>
2021-07-13 15:41:23 +0200

    homework17

---
+++ app/src/main/java/com/tms/app/homework12/Main.kt
@@ -0,0 +1,13 @@
+package com.tms.app.homework12
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Main: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
